5cb4c479604cf338809869e7d69398b5749c93e1
thewart <seth.madlonkay@gmail.com>
2021-11-16 10:35:53 -0500

    model brbs

--- readbrbs.R
+++ readbrbs.R
@@ -1,29 +1,52 @@
-path <- "/run/user/1001/gvfs/smb-share:server=duhsnas-pri.dhe.duke.edu,share=dusom_mooneylab/All_Staff/Seth/For Tom"
+# mntdir <- "/run/user/1001/gvfs/"
+mntdir <- "/run/user/1000/gvfs/"
+path <- paste0(mntdir,"smb-share:server=duhsnas-pri.dhe.duke.edu,share=dusom_mooneylab/All_Staff/Seth/For Tom")
 brbdir <- dir(path) |> str_subset("mat$",negate = T)
 
 brbid <- brbdir[1]
 brblife <- dir(paste(path,brbid,sep="/"))
-brbday <- brblife[1]
-brbdat <- dir(paste(path,brbid,brbday,sep="/"))
-brbstr <- str_split_fixed(brbdat,"_",4)
-brbrun <- unique(brbstr[,3])
 
-brbpre <- paste0(paste(path,brbid,brbday,"/",sep="/"), paste(brbid,brbday,brbrun[1],sep="_"))
+roidat <- data.table()
+metadat <- data.table()
 
-roi <- fread(paste0(brbpre,"_ROIs.csv")) |> t() |> as.data.table()
-roi[,t:=1:.N]
-roi <- melt(roi, id.vars = "t", variable.name = "roi")
+for (d in 1:length(brblife)) {
+  brbday <- brblife[d]
+  brbdat <- dir(paste(path,brbid,brbday,sep="/"))
+  brbstr <- str_split_fixed(brbdat,"_",4)
+  brbrun <- unique(brbstr[,3])
+  
+  for (r in 1:length(brbrun)) {
+    brbpre <- paste0(paste(path,brbid,brbday,"/",sep="/"), paste(brbid,brbday,brbrun[r],sep="_"))
+    
+    rroi <- fread(paste0(brbpre,"_ROIs.csv")) |> t() |> as.data.table()
+    rroi[,t:=1:.N]
+    rroi <- melt(rroi, id.vars = "t", variable.name = "roi")
+    rroi[,value := scale(value),by=roi]
 
-stimtim <- fread(paste0(brbpre,"_stim.csv"))
+    stimtim <- fread(paste0(brbpre,"_stim.csv"))
+    rmetadata <- fread(paste0(brbpre,"_info.csv"))
+    rmetadata$day <- brbday
 
-bs <- 2
-nb <- 5
-
-bindef <- data.table()
-for (i in 1:nrow(stimtim)) {
-  st <- stimtim[i,soundFrames]
-  trng <- st:(bs*nb+st-1)
-  binid <- cut_number(trng,n = nb) %>% as.numeric()
-  binid <- c(sort(-binid),binid-1)
-  bindef <- rbind(bindef, data.table(t=trng,binid=binid))
+    bs <- 5
+    nb <- 1
+    bindef <- data.table()
+    for (t in 1:nrow(stimtim)) {
+      st <- stimtim[t,soundFrames]
+      trng <- st:(bs*nb+st-1)
+      binid <- cut_number(trng,n = nb) %>% as.numeric()
+      binid <- c(sort(-binid),binid-1)
+      trng <- c((st-bs*nb):(st-1),trng)
+      bindef <- rbind(bindef, data.table(t=trng,binid=binid,
+                                         soundID=rep(stimtim[t,soundID],length(binid)),
+                                         stimNum=rep(t,length(binid))))
+    }
+    rroi <- rroi[bindef,on="t"]
+    rroi$run <- rmetadata$run
+    rroi$day <- rmetadata$day
+    
+    roidat <- rbind(roidat, rroi)
+    metadat <- rbind(metadat, rmetadata)
+    
+  }
 }
+
