cb5772236896b64dc33aaa1eb508685e8ee19d60
seth <seth.madlonkay@gmail.com>
2022-01-04 15:07:48 -0500

    beats me

--- readbrbs.R
+++ readbrbs.R
@@ -1,15 +1,19 @@
 mntdir <- switch(Sys.info()['nodename'],
-                 `yawarakai-te`="/run/user/1001/gvfs/",
-                 `seth-Z370P-D3`="/run/user/1000/gvfs/")
+                 `yawarakai-te`="/run/user/1001/gvfs",
+                 `seth-Z370P-D3`="/run/user/1000/gvfs")
 path <- file.path(mntdir,"smb-share:server=duhsnas-pri.dhe.duke.edu,share=dusom_mooneylab","All_Staff","Seth/For Tom")
 
 brbid <- dir(path) |> str_subset("mat$",negate = T)
 b <- brbid[1]
 # brbday <- dir(file.path(path,b))
 
-brbdat <- brbreader(file.path(path,b),cond = "USV_presentation")
+brbdat <- brbreader(file.path(path,b))
 d <- brbdat$meta[,unique(day)[1]]
+c <- brbdat$meta[day==d,unique(condition)[2]]
+r <- brbdat$meta[day==d & condition==c,run]
 # daydat <- dayreader(file.path(path,b,d), cond = "Baseline")
+ggplot(brbdat$roi[day==d & run==r][roi %in% sample(unique(roi),6)][,.(t=t,lum=lum/mean(lum)),by=roi], aes(y=lum,x=t)) + 
+  geom_line() + facet_wrap("roi") + ggtitle(paste(d,c))
 
 redat <- binnify(brbdat$roi[day==d],brbdat$stim[day==d],30,1)
 
@@ -23,6 +27,4 @@ confit <- lmer(paste(baseformula,soundReffs,sep=" + "), data=condat,REML=F)
 bigfit <- lmer(paste(baseformula,"(1|t)",soundReffs,sep=" + "), data=condat,REML=F)
 
 residuals(confit)[condat[,binid==0]] 
-
-
 
--- thebignull.R
+++ thebignull.R
@@ -1,3 +1,4 @@
+d <- brbdat$meta[,unique(day)[3]]
 redat <- binnify(brbdat$roi[day==d],brbdat$stim[day==d],30,1)
 
 nsamp <- 1e3
@@ -6,5 +7,11 @@ for (i in 1:nsamp) {
   permdat <- redat[redat[,unique(postStim),by=.(t,stimNum)][,.(t,permStim=sample(V1)),by=stimNum],on=.(stimNum,t)]
   # permdat <- redat[,.(lum,permStim=sample(postStim,2)),by=.(run,stimNum,roi)]
   t0[,i] <- dcast(permdat,stimNum + roi ~ permStim, value.var="lum")[,t.test(log(`TRUE`)-log(`FALSE`))[[1]],by=roi]$V1
-  # t0[i] <- dcast(permdat,stimNum + roi ~ permStim, value.var="lum")[,mean(`1`/`0`),by=roi][,median(V1-1)][[1]]
+  # t0[,i] <- dcast(permdat,stimNum + roi ~ permStim, value.var="lum")[,t.test(`TRUE`/`FALSE` - 1)[[1]],by=roi]$V1
 }
+
+#population mean test
+apply(t0,2,function(x) t.test(x)[[1]]) |> abs() |> quantile(0.975)
+
+#% significant test
+apply(t0,2,function(x) mean(x>qt(0.975,df = nrow(t0)-1))) |> quantile(0.95)
