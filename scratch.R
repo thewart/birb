04e6f0da483f16718cf2560e539ffe1a5a22adaf
seth <seth.madlonkay@gmail.com>
2022-03-07 15:08:11 -0500

    bunch of stuff

--- scratch.R
+++ scratch.R
@@ -1,11 +1,11 @@
 # mntdir <- switch(Sys.info()['nodename'],
-                 # `yawarakai-te`="/run/user/1001/gvfs",
-                 # `seth-Z370P-D3`="/run/user/1000/gvfs")
+# `yawarakai-te`="/run/user/1001/gvfs",
+# `seth-Z370P-D3`="/run/user/1000/gvfs")
 # path <- file.path(mntdir,"smb-share:server=duhsnas-pri.dhe.duke.edu,share=dusom_mooneylab","All_Staff","Seth/For Tom")
 
 brbid <- dir(path) |> str_subset("mat$",negate = T)
 c <- c("Female_distal_neg","Female_distal_pos","Female_proximal_neg","Female_proximal_pos")
-b <- brbid[2]
+b <- brbid[6]
 brbdat <- brbreader(file.path(path,b),cond = c)
 
 
@@ -64,11 +64,56 @@ fit <- sampling(model,standat,iter=400,chains=1,pars=c("mu_alpha","sigma_alpha",
 
 
 ##### psth
-redat <- binnify(brbdat$roi,brbdat$stim,1,15)
-diffdat <- redat[,.(kHz=soundFrequency[-1]/1e3,winid=winid[-1],delta=diff(log(lum))),
+ldat <- brbdat$roi
+ldat[,lum:=lm(log(lum) ~ 1 + t) |> resid(),by=.(roi,run)]
+redat <- binnify(ldat,brbdat$stim,1,15)
+diffdat <- redat[,.(kHz=soundFrequency[-1]/1e3,winid=winid[-1],delta=diff(lum)),
                  by=.(day,run,stimNum,roi)]
 
 diffdat[winid>-6,mean(delta),by=.(day,run,kHz,winid)][brbdat$meta[,.(day,run,condition)],on=.(day,run)] |> 
   ggplot(aes(x=winid,y=V1,color=ordered(kHz))) + geom_line() + facet_grid(day ~ condition) +
   geom_hline(yintercept = 0,color="gray") + geom_vline(xintercept = 0,color="gray") + 
   ylab("delta") + xlab("timepoint") + ggtitle(b) + theme_classic()
+
+shadup <- lmerControl(check.conv.singular = .makeCC(action = "ignore",tol = formals(isSingular)$tol))
+
+lldat <- data.table()
+manyfits <- list()
+dses <- diffdat[,.(day,kHz)] |> unique()
+for (d in dses[,unique(day)]) {
+  # manyfits[[d]] <- list()
+  ses <- dses[day==d,kHz] |> sort()
+  
+  for (s in ses) {
+    # manyfits[[d]][[paste0(s,"kHz")]] <- list()
+    
+    for (t in -5:14) {
+      
+      cat(paste(d,s,t,sep=":"),"\r")
+      
+      dstdat <- diffdat[day==d & kHz==s & winid==t,.(run,roi,stimNum,delta)]
+      
+      fit1 <- lmer(delta ~ 1 + (1|roi) + (1|stimNum:run),data=dstdat,REML=F,control=shadup)
+      fit0 <- lmer(delta ~ 0 + (1|stimNum:run),data=dstdat,REML=F,control=shadup)
+      
+      if (dstdat[,uniqueN(run)] > 1) {
+        dummified <- model.matrix(~ 0 + run,dstdat)
+        dstdat <- cbind(dstdat,dummified)
+        runs <- paste(colnames(dummified),collapse =" + ")
+        # h2 <- paste("delta ~ 0", runs, "(1 | roi)", paste0("(0 + ",runs," || roi)"),
+        #             "(1 | stimNum:run)",sep = " + ")
+        # fit2 <- lmer(as.formula(h2),data=dstdat,REML=F,control=shadup)
+        fit2 <- lmer(delta ~ 1 + run + (1|roi) + (1|roi:run) + (1|stimNum:run),data=dstdat,REML=F,control=shadup)
+        manyfits[[d]][[paste0(s,"kHz")]][[paste0("t",t)]] <- fit2
+        ll2 <- logLik(fit2) |> as.numeric()
+        
+      } else {
+        
+        ll2 <- NA_real_
+      }
+      
+      lldat <- rbind(lldat,data.table(ll0=logLik(fit0),ll1=logLik(fit1),ll2=as.numeric(ll2),day=d,kHz=s,t=t))
+    }
+  }
+}
+
