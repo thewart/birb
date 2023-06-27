4a817d8bff7835621b5eae2e79ba8dda05ce44c9
thewart <seth.madlonkay@gmail.com>
2023-06-27 13:17:14 -0400

    uh beats me

--- vocal.R
+++ vocal.R
@@ -2,7 +2,8 @@ path <- "~/Desktop/For Tom"
 source("helperfuncs.R")
 mouseid <- dir(path)
 
-thesevars <- c("vocal_delta")
+thesevars <- c("von_onset","shoulder_delta","rpm_delta")
+nothesevars <- c("shoulder_delta","rpm_delta")
 fitdat <- data.table()
 sigdat <- data.table()
 for (thismouse in mouseid) {
@@ -17,7 +18,7 @@ for (thismouse in mouseid) {
   for (thisday in vocsess[,unique(day)]) {
     # thisday <- vocsess[,unique(day)[j]]
     theseruns <- vocsess[day==thisday,run]
-    mousedat <- dayreader(file.path(path,thismouse,thisday),type="roi",run=theseruns)
+    mousedat <- dayreader(file.path(path,thismouse,thisday),run=theseruns)
     
     # vocbouts <- vocbeh[day==thisday,get_bouts(vocal_present),by=.(condition,run)]
     # vocwindows <- vocbouts[,c(dur_bout=dur_bout,slicetime(start_bout,1,15)),by=.(condition,run,num_bout)]
@@ -28,22 +29,25 @@ for (thismouse in mouseid) {
     
     delta_dat <- vocbeh[,.(shoulder_delta=c(NA,diff(shoulder_energy)),
                            rpm_delta=c(NA,diff(running_RPM)),
-                           vocal_delta=c(NA,diff(vocal_rate)),
-                           vocal_onset=c(NA,diff(vocal_present)==1) |> as.numeric(),
-                           vocal_offset=c(NA,diff(vocal_present)==-1) |> as.numeric()),
+                           vrate_delta=c(NA,diff(vocal_rate)),
+                           von_onset=c(NA,diff(vocal_present)==1) |> as.numeric(),
+                           von_offset=c(NA,diff(vocal_present)==-1) |> as.numeric()),
+                           # von_delta=c(NA,diff(vocal_present))),
                         by=.(day,run)]
     delta_dat <- cbind(vocbeh[,.(t)],delta_dat[,c(
       shift(shoulder_delta,-5:15,give.names=T),
-      shift(vocal_delta,-5:15,give.names = T),
+      shift(vrate_delta,-5:15,give.names = T),
       shift(rpm_delta,-5:15,give.names=T),
-      shift(vocal_onset,-5:15,give.names=T),
-      shift(vocal_offset,-5:15,give.names=T)),
+      shift(von_onset,-5:15,give.names=T),
+      shift(von_offset,-5:15,give.names=T)),
+      # shift(von_delta,-5:15,give.names=T)),
       by=.(day,run)])
     
     ldat <- mousedat$roi
     # ldat[,loglum:=fffilt(lum, lo=4, padto = 2.5e4),by=.(run,roi)]
     ldat[,loglum := log(lum) - mean(log(lum)), by= .(run,roi)]
     ldat[,loglum_delta := c(NA,diff(loglum)),by=.(run,roi)]
+    # ldat[,logpil_delta := c(NA,diff(log(pil))),by=.(run,roi)]
     ldat <- cbind(ldat,ldat[,shift(loglum_delta,1:15,give.names = T),by=.(run,roi)][,-c(1,2),with=F])
     # ldat <- cbind(ldat,ldat[,shift(loglum,1:15,give.names = T),by=.(run,roi)][,-c(1,2),with=F])
     
@@ -51,10 +55,10 @@ for (thismouse in mouseid) {
       cat(thismouse, " ", thisday, " ", i,"/",ldat[,uniqueN(roi)], "\r")
       
       covariates <- delta_dat[day==thisday,c("run","t",sapply(thesevars,str_subset,string=names(delta_dat)) |> as.vector()),with=F]
-      fitme <- ldat[roi==paste0("V",i)][covariates,on=.(run,t)][,-c(1:5),with=F]
+      fitme <- ldat[roi==paste0("V",i)][covariates,on=.(run,t)][,-c(1:6),with=F]
       nofitme <- fitme[!is.na(rowSums(fitme))]
       fullmodel <- lm(loglum_delta ~ ., data=nofitme)
-      nullmodel <- lm(loglum_delta ~ ., data=nofitme[,str_detect(names(nofitme),"loglum_delta"),with=F])
+      nullmodel <- lm(loglum_delta ~ ., data=nofitme[,sapply(c("loglum_delta",nothesevars),str_subset,string=names(nofitme)) |> unlist(),with=F])
       cov_info <- str_split_fixed(names(fullmodel$coefficients[-1]),"_",4)
 
       fitdat <- rbind(fitdat,
@@ -79,6 +83,12 @@ wideunits <- dcast(sigunits,formula = roi + day ~ covariate + lag, value.var="ts
 #   ggplot(aes(y=value,x=as.numeric(lag),color=Var2)) + geom_line() + facet_wrap(vars(cov),nrow=1,scales="free_x") + 
 #   geom_hline(yintercept = 0) + theme_cowplot()
 
-ggplot(sigdat,aes(x=p,group=day)) + geom_density() + facet_wrap(vars(mouse),ncol=5)
-fitdat[covariate=="vocal_onset"][sigdat[mouse %in% c("TH134","TH165","TH177") & p<0.05],on=.(day,mouse,roi)] |> 
-  ggplot(aes(y=beta,x=lag,group=roi,color=mouse)) + geom_line(alpha=0.25) + facet_wrap(vars(day),scales="free") + theme_minimal()
+sigdat[,pfdr:=p.adjust(p,method="fdr"),by=.(mouse,day)]
+ggplot(sigdat,aes(x=p)) + geom_histogram() + facet_wrap(vars(mouse),ncol=5)
+fitdat[covariate=="von_onset"][sigdat[pfdr<0.05],on=.(day,mouse,roi)] |> 
+  ggplot(aes(y=beta,x=lag,group=roi,color=mouse)) + geom_line(alpha=0.25) + facet_wrap(vars(paste(mouse,day,sep=":")),scales="free") + theme_minimal()
+fitdat[covariate=="von_onset"][sigdat[pfdr<0.05],on=.(day,mouse,roi)][day %in% sigdat[pfdr<0.05][,.N,by=.(day,mouse)][N>5,day]][,mean(tstat),by=.(mouse,day,lag)] |> 
+  ggplot(aes(y=V1,x=lag,color=mouse)) + geom_line() + facet_wrap(vars(paste(mouse,day,sep=":")),scales="free") + theme_minimal()
+
+fitdat[covariate=="shoulder_delta"] |> ggplot(aes(y=beta,x=lag,group=roi,color=mouse)) + 
+  geom_line(alpha=0.1) + facet_wrap(vars(paste(mouse,day,sep=":")),scales="free") + theme_minimal()
